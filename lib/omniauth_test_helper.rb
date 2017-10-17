dcedfb3c836949f70337aaeef594bf7606706e99
haazime <h4zime@gmail.com>
2017-10-17 18:49:50 +0900

    generate uid when not passed

--- lib/omniauth_test_helper.rb
+++ lib/omniauth_test_helper.rb
@@ -2,14 +2,14 @@ require 'omniauth_test_helper/version'
 
 require 'active_support'
 require 'active_support/core_ext/hash'
-
+require 'securerandom'
 
 module OmniAuthTestHelper
   
   def mock_auth_hash(attributes = {})
     auth_hash = {
       provider: attributes[:provider],
-      uid: attributes[:uid],
+      uid: attributes[:uid] || SecureRandom.uuid,
       info: {
         name: attributes[:name]
       }
--- spec/mock_auth_hash_spec.rb
+++ spec/mock_auth_hash_spec.rb
@@ -14,10 +14,26 @@ RSpec.describe 'mock_auth_hash' do
         uid: 'uid123',
         name: 'Resource Owner'
       )
+
     aggregate_failures do
       expect(auth_hash['provider']).to eq('google_oauth2')
       expect(auth_hash['uid']).to eq('uid123')
       expect(auth_hash['info']['name']).to eq('Resource Owner')
+    end
+  end
+
+  it do
+    auth_hash =
+      mock_auth_hash(
+        provider: 'google_oauth2',
+        name: 'Resource Owner'
+      )
+    uid = auth_hash['uid']
+
+    aggregate_failures do
+      expect(uid).to_not be_nil
+      expect(uid).to be_instance_of(String)
+      expect(uid.size).to be > 1
     end
   end
 end
